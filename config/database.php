685d44ab2ca11bb4f7ce030c79a973530282f0b1
Prohor722 <prohor.b722@gmail.com>
2022-12-16 01:08:26 +0600

    databse.php initial configration

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('mysql://root:rIgLkLFhsZdqNZiJTkDJ@containers-us-west-68.railway.app:6844/railway'),
-            'host' => env('DB_HOST', 'containers-us-west-68.railway.app'),
-            'port' => env('DB_PORT', '6844'),
-            'database' => env('DB_DATABASE', 'railway'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'rIgLkLFhsZdqNZiJTkDJ'),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
